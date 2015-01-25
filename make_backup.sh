e03cd1cc98febea4ab192d8e6da4230ce446f602
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-01-25 13:45:58 +0100

    T688: Automatyczne tworzenie backupów na serwerze dev.spistresci.pl nie działa

--- make_backup.sh
+++ make_backup.sh
@@ -2,10 +2,12 @@
 cd project/
 
 fig stop
-archive_filename=devblog-backup.tar.bz2
+archive_filename=devblog-backup.tar
 
-docker run -i -t --volumes-from project_data_1 -v $(pwd):/backup/ ubuntu tar jcvf /backup/${archive_filename} /var/lib/postgresql/data /mezzanine-project/static/media
+docker run --volumes-from project_data_1 -v $(pwd):/backup/ ubuntu tar cvf /backup/${archive_filename} /var/lib/postgresql/data /mezzanine-project/static/media
 
 fig -f fig-production.yml up -d
 
 bakthat backup --prompt no $archive_filename
+
+rm $archive_filename
--- restore_backup.sh
+++ restore_backup.sh
@@ -2,9 +2,11 @@
 cd project/
 
 fig stop
-archive_filename=devblog-backup.tar.bz2
+archive_filename=devblog-backup.tar
 
 bakthat restore $archive_filename
-docker run --volumes-from project_data_1 -v $(pwd):/backup ubuntu tar -vxjf /backup/${archive_filename}
+docker run --volumes-from project_data_1 -v $(pwd):/backup ubuntu tar -vxf /backup/${archive_filename}
 
 fig -f fig-production.yml up -d
+
+rm $archive_filename
